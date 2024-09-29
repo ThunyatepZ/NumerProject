9ea5cae6517fb4a3d00eb35e084987def5146c3c
ThunyatepZ <taptitan4@gmail.com>
2024-09-30 02:58:06 +0700

    add loading(unfinish)

--- swk/src/page/Graphical.jsx
+++ swk/src/page/Graphical.jsx
@@ -1,116 +1,146 @@
 import { all, create } from 'mathjs';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import GraphicalJS from '../CalculateFornt/Graphical';
 import MathEquation from '../component/Boxmath';
 import Graphishow from '../component/graph';
+import CircularIndeterminate from '../component/loading';
 import Submenuroot from '../component/submenu.root';
 import BasicTable from '../component/Table';
-const math = create(all)
-// const postGPC = "http://localhost:3000/api/GraphicalMTD/Post"
+
+const math = create(all);
 
 function Graphical() {
-    const [FX,setFX] = useState("")
-    const [Xstart,setXstart] = useState("")
-    const [Xend,setXend] = useState("")
-    const [Error,setError] = useState("")
-    const [form,setform] = useState({})
-    const [name , Setname] = useState("Anser")
+    const [FX, setFX] = useState("");
+    const [Xstart, setXstart] = useState("");
+    const [Xend, setXend] = useState("");
+    const [Error, setError] = useState("");
+    const [form, setForm] = useState({});
+    const [name, setName] = useState("Anser");
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true)
     const equation = 'f(x) = ';
 
-    const FXchange = ({target: {value}}) =>{
-        console.log(value)
-        setFX(value)
-    }
-    const XstartChange = ({target: {value}}) =>{
-        console.log(value)
-        setXstart(value)
-    }
-    const Xendchange = ({target: {value}}) =>{
-        console.log(value)
-        setXend(value)
-    }
-    const Errorchange = ({target: {value}}) =>{
-        console.log(value)
-        setError(value)
-    }
-    const [ANS,SetANS] = useState({})
-    
+    useEffect(() => {
+        const loadData = () => {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve('Loading data...');
+                }, 2000);
+            });
+        };
 
-    const handlechangeforequation = (e) =>{
-        setFX(e.target.value)
-    }
+        loadData().then((result) => {
+            setData(result);
+            setLoading(false);
+        });
+    }, []);
 
-    const handlechange = (e) =>{
-        setform({
+    const handleChangeForEquation = (e) => {
+        setFX(e.target.value);
+    };
+
+    const handleChange = (e) => {
+        setForm({
             ...form,
             [e.target.name]: e.target.value
-        })
-        
-    }
+        });
+    };
 
-    const handlesubmit = async(e)=>{
-        
-        e.preventDefault()
-        //console.log(form)
-        let G = await GraphicalJS(form)
-        Setname(G)
-        name.xans.sort((a,b) => a-b)
-        
-    }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        let G = await GraphicalJS(form);
+        setName(G);
+        name.xans.sort((a, b) => a - b);
+    };
 
-    
-
-
-
-
-return (
-    <div className='bg-#1D232A'>
-    
-    <div className='text-center text-3xl'>
-        <h1 className='text-white pt-10 pb-5'>Root of equation : Graphical Method</h1>
-        <div className="divider divider-neutral"></div>
-        
-    </div>
-    <div className='text-center flex justify-center text-white'>
-        <MathEquation equation={equation} />
-        <MathEquation equation={FX} />
-    </div>
-        <div className='text-center pt-5'>
-        
-        <form onSubmit={handlesubmit}>
-        <input type="text" name='equation' className='text-center bg-white py-3 mr-4 text-black rounded-md' onChange={(e)=> {
-            handlechangeforequation(e) , handlechange(e)}} placeholder='f(x) = 4x-32'/><br />
-        <input type="text" name='Xstart' className='text-center bg-white py-3 mt-2 mr-4 text-black rounded-md' onChange={(e)=> handlechange(e)} placeholder='Xstart'/>
-        <input type="text" name='Xend' className='text-center bg-white py-3 mr-4 text-black rounded-md' onChange={(e)=> handlechange(e)} placeholder='Xend'/>
-        <input type="text" name='Error' className='text-center bg-white py-3 mt-2 mr-4 text-black rounded-md' onChange={(e)=> handlechange(e)} placeholder='Error'/>
-        <br /><button type='submit' className='bg-green-400 text-black p-3 rounded mt-3'>send</button>
-        </form>
-
-
-        </div>
-        <div className='text-center'>
-        {/* <br />[equation:{ANS.equation} Xstart:{ANS.Xstart} Xend:{ANS.Xend} Error:{ANS.Error}]<br/> */}
-        <br />Anser : {name.NewAnser}<br/>
-        </div>
-
-        <div className='text-center text-white'>
-            <Submenuroot/>
-        </div>
-
-
-        <div className='w-full flex justify-center items-center'>
-            <div className='rounded-lg border border-black overflow-hidden'>
-            <Graphishow className='rounded-md border border-black' x ={name.xans} y ={name.yans} check={true} maingraphx={name.Mgx} maingraphy={name.Mgy}/>
+    return (
+        <div className='bg-#1D232A'>
+            <div className='text-center text-3xl'>
+                <h1 className='text-white pt-10 pb-5'>Root of equation : Graphical Method</h1>
+                <div className="divider divider-neutral"></div>
             </div>
 
-        </div>
-        <div className='w-full flex justify-center items-center mt-10'>
-                <div className='w-[70%]'>
-                    <BasicTable  x={name.xans} y ={name.yans} errorFAC={name.err} iterative={name.it}/>
+
+            {loading ? (
+                <div className='text-center text-white flex justify-items-center items-center justify-center'><CircularIndeterminate/></div> // แสดงข้อความการโหลด
+            ) : (
+                <div>
+                    <div className='text-center flex justify-center text-white'>
+                        <MathEquation equation={equation} />
+                        <MathEquation equation={FX} />
+                    </div>
+                    <div className='text-center pt-5'>
+                        <form onSubmit={handleSubmit}>
+                            <input
+                                type="text"
+                                name='equation'
+                                className='text-center bg-white py-3 mr-4 text-black rounded-md'
+                                onChange={(e) => { handleChangeForEquation(e); handleChange(e); }}
+                                placeholder='f(x) = 4x-32'
+                            /><br />
+                            <input
+                                type="text"
+                                name='Xstart'
+                                className='text-center bg-white py-3 mt-2 mr-4 text-black rounded-md'
+                                onChange={handleChange}
+                                placeholder='Xstart'
+                            />
+                            <input
+                                type="text"
+                                name='Xend'
+                                className='text-center bg-white py-3 mr-4 text-black rounded-md'
+                                onChange={handleChange}
+                                placeholder='Xend'
+                            />
+                            <input
+                                type="text"
+                                name='Error'
+                                className='text-center bg-white py-3 mt-2 mr-4 text-black rounded-md'
+                                onChange={handleChange}
+                                placeholder='Error'
+                            />
+                            <br />
+                            <button type='submit' className='bg-green-400 text-black p-3 rounded mt-3'>Send</button>
+                        </form>
+                    </div>
+
+                    <div className='text-center mt-2'>
+                        {name.xans && (
+                            `Anser ${name.NewAnser}`
+                        )}
+                    </div>
+
+                    <div className='text-center text-white'>
+                        <Submenuroot />
+                    </div>
+
+                    <div className='w-full flex justify-center items-center'>
+                        <div className='rounded-lg border border-black overflow-hidden'>
+                            <Graphishow
+                                className='rounded-md border border-black'
+                                x={name.xans}
+                                y={name.yans}
+                                check={true}
+                                maingraphx={name.Mgx}
+                                maingraphy={name.Mgy}
+                            />
+                        </div>
+                    </div>
+
+                    <div className='w-full flex justify-center items-center mt-10'>
+                        <div className='w-[70%]'>
+                            <BasicTable
+                                x={name.xans}
+                                y={name.yans}
+                                errorFAC={name.err}
+                                iterative={name.it}
+                            />
+                        </div>
+                    </div>
                 </div>
+            )}
         </div>
-    </div>
-)
+    );
 }
 
-export default Graphical
+export default Graphical;
