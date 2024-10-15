0999c83bbb618b873f767eff4cc88988f33c517a
ThunyatepZ <taptitan4@gmail.com>
2024-10-15 22:43:49 +0700

    a

--- server/Models/config/db.js
+++ server/Models/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const conectDB = async () =>{
     try{
-        await mongoose.connect('mongodb+srv://taptitan42:2CMfmKUSfn1xNXOP@fornumer.wdxpi.mongodb.net/Numer?retryWrites=true&w=majority')
+        await mongoose.connect('mongodb+srv://taptitan42:Thunyatep15@fornumer.wdxpi.mongodb.net/Numer?retryWrites=true&w=majority')
         console.log('DB conect')
     }catch(err){
         console.log(err)
