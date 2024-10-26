1dbcebd9adc09ce3707b5f3d5b0038d9572152e3
Thunyatep <taptitan4@gmail.com>
2024-10-26 23:32:46 +0700

    add docker

--- swk/vite.config.js
+++ swk/vite.config.js
@@ -4,4 +4,11 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true,
+    },
+  }
 })
